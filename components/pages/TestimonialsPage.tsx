1c2a888a5fefdfd43c64122ad9404c2c492990a5
Anton K <steambot33@gmail.com>
2026-04-20 08:18:12 +0300

    feat(testimonials): replace placeholder with real testimonial cards

--- components/pages/TestimonialsPage.tsx
+++ components/pages/TestimonialsPage.tsx
@@ -22,15 +22,107 @@ export default function TestimonialsPage() {
       <main>
         {breadcrumbs.length > 0 && <Breadcrumbs items={breadcrumbs} />}
         <HeroSection
-          title="Testimonials"
-          description="Hundreds of satisfied Setproduct buyers already boosted an entire workflow, development and sprints by using our products."
+          title="Our satisfied Customers"
+          description="We helped more than 3,000 designers, developers and startups to design faster and better"
         />
         <div className="section">
-          <div className="container">
-            <div className="testimonials_list">
-              <p className="text-size-medium">
-                Testimonial content is dynamically loaded from external sources. Visit our Gumroad page to see customer reviews.
-              </p>
+          <div className="section-padding top-80 bottom-80">
+            <div className="container">
+              <div className="testimonials_sect">
+                <div className="testimonials_cl-wr w-dyn-list">
+                  <div className="testimonials_cl w-dyn-items">
+                    <div className="testimonials_cl-item w-dyn-item">
+                      <a
+                        className="lightbox-link-with-zoom w-inline-block"
+                        href="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7edafad7f95897a9370_DanJasnowski_testimo.avif"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <img
+                          src="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7edafad7f95897a9370_DanJasnowski_testimo.avif"
+                          srcSet="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7edafad7f95897a9370_DanJasnowski_testimo.avif 500w, /external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7edafad7f95897a9370_DanJasnowski_testimo.avif"
+                          alt="Dan Jasnowski testimonial"
+                          loading="lazy"
+                        />
+                      </a>
+                    </div>
+                    <div className="testimonials_cl-item w-dyn-item">
+                      <a
+                        className="lightbox-link-with-zoom w-inline-block"
+                        href="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7dd29303a3832d8831e_Craig_Revi_-_Feb_27_.avif"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <img
+                          src="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7dd29303a3832d8831e_Craig_Revi_-_Feb_27_.avif"
+                          srcSet="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7dd29303a3832d8831e_Craig_Revi_-_Feb_27_.avif 500w, /external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7dd29303a3832d8831e_Craig_Revi_-_Feb_27_.avif"
+                          alt="Craig testimonial"
+                          loading="lazy"
+                        />
+                      </a>
+                    </div>
+                    <div className="testimonials_cl-item w-dyn-item">
+                      <a
+                        className="lightbox-link-with-zoom w-inline-block"
+                        href="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7c64c53ed2ebfd26575_Berc_Topcu_-_Mar_15_.avif"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <img
+                          src="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7c64c53ed2ebfd26575_Berc_Topcu_-_Mar_15_.avif"
+                          alt="Berc Topcu testimonial"
+                          loading="lazy"
+                        />
+                      </a>
+                    </div>
+                    <div className="testimonials_cl-item w-dyn-item">
+                      <a
+                        className="lightbox-link-with-zoom w-inline-block"
+                        href="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7b96be8fabc4f9ce662_Ash_-_Jun_2_2021.avif"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <img
+                          src="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7b96be8fabc4f9ce662_Ash_-_Jun_2_2021.avif"
+                          srcSet="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7b96be8fabc4f9ce662_Ash_-_Jun_2_2021.avif 500w, /external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7b96be8fabc4f9ce662_Ash_-_Jun_2_2021.avif"
+                          alt="Ash testimonial"
+                          loading="lazy"
+                        />
+                      </a>
+                    </div>
+                    <div className="testimonials_cl-item w-dyn-item">
+                      <a
+                        className="lightbox-link-with-zoom w-inline-block"
+                        href="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7a933cff09f4d9ee160_Anqi_L_18-10-22-min.avif"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <img
+                          src="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7a933cff09f4d9ee160_Anqi_L_18-10-22-min.avif"
+                          srcSet="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7a933cff09f4d9ee160_Anqi_L_18-10-22-min.avif 500w, /external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce7a933cff09f4d9ee160_Anqi_L_18-10-22-min.avif"
+                          alt="Anqi L testimonial"
+                          loading="lazy"
+                        />
+                      </a>
+                    </div>
+                    <div className="testimonials_cl-item w-dyn-item">
+                      <a
+                        className="lightbox-link-with-zoom w-inline-block"
+                        href="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce78df8de29b8f399f74c_Andrzej_-_Mar_3_2021.avif"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <img
+                          src="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce78df8de29b8f399f74c_Andrzej_-_Mar_3_2021.avif"
+                          srcSet="/external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce78df8de29b8f399f74c_Andrzej_-_Mar_3_2021.avif 500w, /external/cdn.prod.website-files.com/64d1f4894b9a964bb3b26df9/654ce78df8de29b8f399f74c_Andrzej_-_Mar_3_2021.avif"
+                          alt="Andrzej testimonial"
+                          loading="lazy"
+                        />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
